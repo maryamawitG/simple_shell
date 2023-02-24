f6a7a2b10007ee46c6ca8ee4ab24df4b77e747dc
maryamawitG <maryamwit4kidus@gmail.com>
2023-02-24 14:05:50 -0800

    decond team project

--- splites_string.c
+++ splites_string.c
@@ -6,6 +6,7 @@
  * @d: the delimeter string
  * Return: a pointer to an array of strings, or NULL on failure
  */
+
 char **strtow(char *str, char *d)
 {
 	int i, j, k, m, numwords = 0;
@@ -18,34 +19,34 @@ char **strtow(char *str, char *d)
 	for (i = 0; str[i] != '\0'; i++)
 		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
 			numwords++;
-				if (numwords == 0)
-					return (NULL);
-				s = malloc((1 + numwords) * sizeof(char *));
-				if (!s)
-					return (NULL);
-				for (i = 0, j = 0; j < numwords; j++)
-				{
-					while (is_delim(str[i], d))
-						i++;
-					k = 0;
-					while (!is_delim(str[i + k], d) && str[i + k])
-						k++;
-					s[j] = malloc((k + 1) * sizeof(char));
-				if (!s[j])
-				{
-					for (k = 0; k < j; k++)
-					{
-						free(s[k]);
-						free(s);
-						return (NULL);
-					}
-					for (m = 0; m < k; m++)
-						s[j][m] = str[i++];
-					s[j][m] = 0;
-				}
-				s[j] = NULL;
-				return (s);
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
+		while (is_delim(str[i], d))
+			i++;
+		k = 0;
+		while (!is_delim(str[i + k], d) && str[i + k])
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
 }
+
 /**
  * **strtow2 - splits a string into words
  * @str: the input string
@@ -74,7 +75,7 @@ char **strtow2(char *str, char d)
 		while (str[i] == d && str[i] != d)
 			i++;
 		k = 0;
-		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+	       	while (str[i + k] != d && str[i + k] && str[i + k] != d)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
@@ -91,4 +92,3 @@ char **strtow2(char *str, char d)
 	s[j] = NULL;
 	return (s);
 }
-
